364742a1f2c6c26deb80cdf56bc3a761fd9583d5
Branden Butler <bwtbutler@hotmail.com>
2022-07-11 21:54:18 -0500

    Add mrl dataclasses in utils

--- mrlpy/utils.py
+++ mrlpy/utils.py
@@ -1,13 +1,55 @@
 import string
 import random
+from mrlpy.framework.deserializer import mrl_dataclass
+from mrlpy.meventdispatch import MEventDispatch
+from mrlpy.mevent import Message
 '''
 Utility methods and variables
 '''
 
+eventDispatch = MEventDispatch()
+
+@mrl_dataclass
+class DescribeResults(object):
+    id: str
+    uuid: str
+    request: dict
+    platform: object
+    status: object
+    registrations: list
+
+    def __init__(self):
+        self.id = ""
+        self.uuid = ""
+        self.request = {}
+        self.platform = None
+        self.status = None
+        self.registrations = []
+
+
+@mrl_dataclass
+class MRLListener(object):
+    topicMethod: str
+    callbackName: str
+    callbackMethod: str
+
+    def __call__(self, ev):
+        message = Message(self.callbackName, self.callbackMethod, ev.data)
+        eventDispatch.dispatch_event(message)
+
+    def __hash__(self) -> int:
+        return 37 + self.topicMethod.__hash__() + self.callbackName.__hash__() + self.callbackMethod.__hash__()
+
+
+@mrl_dataclass
+class DescribeQuery(object):
+    id: str
+    uuid: str
+    platform: object
 
 
 def genID(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
-	'''
-	Generate a random ID for creating unique names
-	'''
-	return ''.join(random.choice(chars) for _ in range(size))
+    '''
+    Generate a random ID for creating unique names
+    '''
+    return ''.join(random.choice(chars) for _ in range(size))
